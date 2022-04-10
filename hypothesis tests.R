51c7ff0af26c5e8809bade2cb7e5bf8d670d84f5
Apostolos Stamenos <astamenos@gmail.com>
2022-04-10 14:41:40 -0400

    Adding code for the (equal variances) pooled t-test and confidence interval

--- hypothesis tests.R
+++ hypothesis tests.R
@@ -4,7 +4,6 @@
 
 ## Load required libraries
 library(tidyverse)
-library(ggplot2)
 library(data.table)
 
 ## =========================================== PART 1 ===============================================
@@ -21,12 +20,30 @@ nonsmoker_df <- data %>% filter(currentSmoker == 0)
 alpha <- 0.05
 
 ## Pooled Two Sample t-test p-value method
+n1 <- length(nonsmoker_df$sysBP)
+n2 <- length(smoker_df$sysBP)
+df <- n1 + n2 - 2
+
+ybar_1 <- sum(nonsmoker_df$sysBP)/n1
+ybar_2 <- sum(smoker_df$sysBP)/n2
+diff <- ybar_1 - ybar_2
+D_0 <- 0
+
+s_sq1 <- sum((nonsmoker_df$sysBP-ybar_1)^2)/(n1-1)
+s_sq2 <- sum((smoker_df$sysBP-ybar_2)^2)/(n2-1)
+s_sq_p <- ((n1-1)*s_sq1+(n2-1)*s_sq2)/df
+
+se <- (sqrt(s_sq_p)*sqrt(1/n1+1/n2))
+T <- (diff - D_0)/se
+p_val <- 2*pt(T, df)
 
 ## Pooled Two Sample t-test confidence interval method
+t_quants <- qt(c(alpha/2, 1-alpha/2), df)
+CI <- diff+se*t_quants
 
 ## Satterthwaite Approximation Two Sample t-test p-value method
 
-## Satterthwaite Approxiamtion Two Sample t-test confidence interval method
+## Satterthwaite Approximation Two Sample t-test confidence interval method
 
 
 ## =========================================== PART 2 ===============================================
@@ -41,3 +58,4 @@ n2 <- c(10, 30, 70)
 
 # True mean difference mu1 - mu2
 true_mean_diff <- c(-5, -1, 1, 5)
+
